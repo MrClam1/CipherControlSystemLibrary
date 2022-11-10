4ec36b38a67cc4768c366620dd9b2e9e98ea9e8b
Sanal Boldyrev <MrClam1@yandex.ru>
2022-11-10 17:15:32 +0300

    add files for cipher

---
+++ src/cipher/include/ccsl_cipher.h
@@ -0,0 +1,11 @@
+#ifndef CCSL_CIPHER_H
+#define CCSL_CIPHER_H
+
+namespace CCSL::Cipher
+{
+
+
+
+}
+
+#endif // !CCSL_CIPHER_H
--- src/types/include/ccsl_types.h
+++ src/types/include/ccsl_types.h
@@ -1,6 +1,22 @@
 #ifndef CCSL_TYPES_H
 #define CCSL_TYPES_H
 
+#include <iostream>
 
+namespace CCSL::Types
+{
+	template<class T>
+	struct _ByteBuffer
+	{
+	public:
+		_ByteBuffer();
+
+	private:
+		std::unique_ptr<T[]> _buffer;
+		size_t _size = 0;
+
+	};
+
+}
 
 #endif // !CCSL_TYPES_H
